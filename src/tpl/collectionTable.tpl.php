f70a3b5c8f75f20b3c5869a69450aa9609f02adc
Yosko <yosko@yosko.net>
2022-10-20 16:04:25 +0200

    separated celle definition from table (reusability)

--- src/tpl/collectionTable.tpl.php
+++ src/tpl/collectionTable.tpl.php
@@ -49,48 +49,8 @@ if (!empty($data)) { ?>
             $value = $instance->$key ?? null;
         //foreach ($instance as $key => $value) {
             // passwords are hidden
-            if ($breadView->isPropertyReadable($instance, $key) && $breadView->getPropertyType($instance, $key) == BreadManager::TYPE_PASSWORD) { ?>
-
-                <td>*****</td><?php
-            } elseif ($breadView->isPropertyReadable($instance, $key) && !$breadView->isPropertySecondary($instance, $key)) {
-                $fgColor = $breadView->getForegroundColor($instance, $key);
-                $bgColor = $breadView->getBackgroundColor($instance, $key);
-                $type = $breadView->getPropertyType($instance, $key);
-
-                if ($type == BreadManager::TYPE_TEXT_MULTI && !empty($instance->$key) && strlen($instance->$key) > 10)
-                    $value = mb_strimwidth($instance->$key, 0, 30, "...");
-                else
-                    $value = $breadView->formated($instance, $key);
-
-                $link = $breadView->getHyperlink($instance, $key);
-
-                $node = $dom->createElement('td');
-                $textNode = $dom->createTextNode($value);
-                if (empty($link))
-                    $node->appendChild($textNode);
-                else {
-                    $linkNode = $dom->createElement('a');
-                    $linkNode->setAttribute('href', $link);
-                    $linkNode->appendChild($textNode);
-                    $node->appendChild($linkNode);
-                }
-
-                if ($type == BreadManager::TYPE_TEXT_MULTI) {
-                    $node->setAttribute('title', $breadView->formated($instance, $key));
-                    $node->setAttribute('class', 'multiline');
-                }
-
-                if (!empty($fgColor) || !empty($bgColor)) {
-                    $color = '';
-                    if (!empty($fgColor))
-                        $color .= 'color: '.$fgColor.'; ';
-                    if (!empty($bgColor))
-                        $color .= 'background-color: '.$bgColor.'; ';
-                    $node->setAttribute('style', $color);
-                }
-
-                echo $dom->saveHTML($node);
-            }
+            
+            include $pluginTplPath.'collectionCell.tpl.php';
 
         } ?>
 
